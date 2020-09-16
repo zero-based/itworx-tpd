5c6ec8cfbf47645d7702c2c542ff0908048f8393
MonicaTanios <monicatanios@gmail.com>
2020-09-16 23:10:25 +0200

    [CLIENT] Add Login/Logout

---
+++ client/pages/login.tsx
@@ -0,0 +1,62 @@
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { useLoginMutation } from "../generated/graphql";
+
+import React from "react";
+import { Button, Form, Input } from "antd";
+import { useRouter } from "next/dist/client/router";
+import { withUrqlClient } from "next-urql";
+
+interface loginProps {}
+
+const Login: React.FC<loginProps> = ({}) => {
+  const router = useRouter();
+  const [, login] = useLoginMutation();
+  return (
+    <Form
+      name="basic"
+      initialValues={{ email: "", password: "" }}
+      onFinish={async (values) => {
+        const response = await login({
+          email: values.email,
+          password: values.password,
+        });
+        if (response.data == null) {
+          console.log("[ERROR] ", response.error?.message);
+        } else {
+          router.push("/");
+        }
+      }}
+    >
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[{ required: true, message: "Please input your email!" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item style={{ textAlign: "center" }}>
+        <Button
+          type="primary"
+          htmlType="submit"
+          style={{
+            background: "#C63527",
+            borderColor: "#C63527",
+          }}
+        >
+          Login
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default withUrqlClient(createUrqlClient)(Login);
--- client/utils/createUrqlClient.ts
+++ client/utils/createUrqlClient.ts
@@ -1,7 +1,37 @@
+import { LoginMutation, MeDocument, MeQuery } from "../generated/graphql";
+
+import { cacheExchange } from "@urql/exchange-graphcache";
 import { dedupExchange, fetchExchange } from "urql";
+import { updateQuery } from "./updateQuery";
 
 export const createUrqlClient = (ssrExchange: any) => ({
   url: process.env.NEXT_PUBLIC_API_URL as string,
   fetchOptions: { credentials: "include" as const },
-  exchanges: [dedupExchange, ssrExchange, fetchExchange],
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            updateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (!result.login) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    ssrExchange,
+    fetchExchange,
+  ],
 });
