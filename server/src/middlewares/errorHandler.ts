7454bde729b84a31d969ebe64a7112a8688df34e
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2020-09-18 11:48:14 +0200

    [SERVER] Fix Error Handler
     * Add type checking.
     * Unify type to be an array of FieldError.

--- server/src/middlewares/errorHandler.ts
+++ server/src/middlewares/errorHandler.ts
@@ -1,26 +1,38 @@
-import { InvalidCredentialsError } from "../errors/InvalidCredentials";
-
 import { ArgumentValidationError } from "type-graphql";
 import { GraphQLError } from "graphql";
 import { ValidationError } from "apollo-server-express";
 
-export const errorHandler = (error: GraphQLError) => {
-  if (error.originalError instanceof ArgumentValidationError) {
+import { InvalidCredentialsError } from "../errors/InvalidCredentials";
+
+export const errorHandler = (error: GraphQLError): any => {
+  return fieldErrorHandler(error) ?? error;
+};
+
+/**
+ * Handles errors that are returned to the client as
+ * FieldError to be used in forms.
+ */
+const fieldErrorHandler = (error: GraphQLError): FieldError[] | null => {
+  var originalError = error.originalError;
+  if (originalError instanceof ArgumentValidationError) {
     const validationErrors = error.extensions!.exception.validationErrors;
     return validationErrors.map(
-      ({ property, constraints }: ValidationError): FieldError => ({
+      ({ property, constraints }: ValidationError) => ({
         field: property,
         message: constraints
           ? (Object.values(constraints)[0] as string)
           : "unknown error",
       })
     );
-  } else if (error.originalError instanceof InvalidCredentialsError) {
-    const property = error.originalError.property;
-    return {
-      field: property,
-      message: "incorrect " + property,
-    } as FieldError;
+  } else if (originalError instanceof InvalidCredentialsError) {
+    const property = originalError.property;
+    return [
+      {
+        field: property,
+        message: "incorrect " + property,
+      },
+    ];
   }
-  return error;
+
+  return null;
 };
