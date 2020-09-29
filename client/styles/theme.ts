5b3b7429bd8682248502eafb9ff513d56cb4c53b
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2020-09-29 17:50:33 +0200

    [CLIENT] Refactor & Restyle MainLayout Components

--- client/styles/theme.ts
+++ client/styles/theme.ts
@@ -5,10 +5,20 @@ const primitives: Partial<ThemePrimitives> = {
   primary: "#C63527", // ITWorx Red
   primary700: "#7C2529", // ITWorx Dark Red
   accent: "#171C8F", // ITWorx Blue
+  accent700: "#101464",
+  mono500: "#D0D3D4", // ITWorx Grey
+  mono1000: "#101820" // ITWorx Black
 };
 
 const overrides = {
-  colors: {},
+  colors: {
+    buttonSecondaryFill: primitives.accent,
+    buttonSecondaryText: "#FFF",
+    buttonSecondaryHover: primitives.accent700,
+    buttonMinimalFill: "#FFF",
+    buttonMinimalText: primitives.primary700,
+    buttonMinimalHover: "#EEE"
+  },
 };
 
 export const ItworxTheme = createTheme(primitives, overrides);
