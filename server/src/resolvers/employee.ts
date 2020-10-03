dcbaf89af16ee295ef07260723dbc3b7ec4dcb7d
MichelineMedhat <michelinemedhat@gmail.com>
2020-10-03 22:19:44 +0200

    [SERVER] Update Employee Resolver

--- server/src/resolvers/employee.ts
+++ server/src/resolvers/employee.ts
@@ -1,5 +1,5 @@
 import { EmployeesProfiles } from "../entities/EmployeesProfiles";
-import { Authorized, Query, Resolver } from "type-graphql";
+import { Arg, Authorized, Query, Resolver } from "type-graphql";
 import { UserRole as R } from "../types/UserRole";
 
 @Resolver()
@@ -10,6 +10,24 @@ export class EmployeeResolver {
     return await EmployeesProfiles.find({
       where: {
         directManagerId: null,
+      },
+    });
+  }
+
+  @Authorized(R.ADMIN, R.MANAGER)
+  @Query(() => [EmployeesProfiles])
+  employeesProfiles(): Promise<EmployeesProfiles[]> {
+    return EmployeesProfiles.find({});
+  }
+
+  @Authorized(R.ADMIN, R.MANAGER)
+  @Query(() => [EmployeesProfiles])
+  managerEmployees(
+    @Arg("directManagerId", () => String) directManagerId: string
+  ): Promise<EmployeesProfiles[]> {
+    return EmployeesProfiles.find({
+      where: {
+        directManagerId: directManagerId,
       },
     });
   }
