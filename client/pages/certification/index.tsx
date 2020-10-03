56279702315f0a629c9dfc59c9e2c390b92d5c6b
MonicaTanios <36570681+MonicaTanios@users.noreply.github.com>
2020-10-04 00:32:40 +0200

    [CLIENT] Add Missing Export Buttons (#84)

--- client/pages/certification/index.tsx
+++ client/pages/certification/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { CertificationTable } from "../../components/certifications/CertificationTable";
-import { PageLayout } from "../../components/common/PageLayout";
+import { CSVLink } from "react-csv";
+
+import { Button } from "baseui/button";
 import {
   Certifications,
   useCertificationsQuery,
   UserRole,
 } from "../../graphql/types";
+import { CertificationTable } from "../../components/certifications/CertificationTable";
+import { PageLayout } from "../../components/common/PageLayout";
 import { withAuth } from "../../hocs/withAuth";
 
 const ViewCertification: React.FC<{}> = () => {
@@ -16,12 +19,37 @@ const ViewCertification: React.FC<{}> = () => {
     },
   });
 
+  const certifications = data?.certifications?.data?.items;
+
+  const csvHeaders = [
+    { label: "Certification Name", key: "certificationName" },
+    {
+      label: "Certification Provider",
+      key: `certificationProvider.certificationProviderName`,
+    },
+    { label: "Talents Count", key: `employeeCertifications.length` },
+  ];
+
   return (
     <PageLayout
       title="Certifications"
       loading={fetching}
       error={!!data?.certifications?.errors}
       contentStyle={{ height: "65vh" }}
+      action={
+        certifications ? (
+          <Button>
+            <CSVLink
+              data={certifications}
+              filename="Certifications.csv"
+              style={{ color: "white", textDecoration: "none" }}
+              headers={csvHeaders}
+            >
+              Export
+            </CSVLink>
+          </Button>
+        ) : null
+      }
     >
       <CertificationTable
         data={data?.certifications?.data?.items as Certifications[]}
