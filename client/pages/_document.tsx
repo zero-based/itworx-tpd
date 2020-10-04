012d2674a0f311de22329d1ca2288d649e4647c9
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2020-10-04 18:45:16 +0200

    [CLIENT] Add favicon & title

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -18,6 +18,25 @@ class MainDocument extends Document<{ stylesheets: Sheet[] }> {
     return (
       <Html>
         <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+
           {this.props.stylesheets.map((sheet, i) => (
             <style
               className="_styletron_hydrate_"
